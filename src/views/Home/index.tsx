e710db15b663f58f522163eea9464091466f2f17
Matheus Costa <matheus271200.ms@gmail.com>
2020-12-18 22:46:35 -0300

    feat: :zap: importing and using component Footer

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -3,6 +3,7 @@ import {SafeAreaView, StyleSheet, ScrollView, StatusBar} from 'react-native';
 
 import Header from './Components/Header';
 import Map from './Components/Map';
+import Footer from './Components/Footer';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import MapboxGL from '@react-native-mapbox-gl/maps';
@@ -21,6 +22,7 @@ const Home = () => {
           style={styles.scrollView}>
           <Header />
           <Map />
+          <Footer />
         </ScrollView>
       </SafeAreaView>
     </>
@@ -31,42 +33,6 @@ const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: Colors.lighter,
     padding: 20,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-  map: {
-    flex: 1,
   },
 });
 
