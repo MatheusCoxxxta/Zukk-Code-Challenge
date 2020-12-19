8b877f4eb9e359a7401323b833914a8bed91f6e5
Matheus Costa <matheus271200.ms@gmail.com>
2020-12-18 22:14:30 -0300

    style: :art: creating some styles to home page

---
+++ src/views/Home/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Title = styled.Text`
+  font-size: 23px;
+  color: #000;
+  font-weight: bold;
+  margin: 0 0 10px 0;
+`;
+
+const SubHeading = styled.Text`
+  font-size: 15px;
+  color: #000;
+  margin: 0 0 10px 0;
+`;
+
+const MapParentBox = styled.View`
+  height: 500px;
+`;
+
+export {Title, SubHeading, MapParentBox};
