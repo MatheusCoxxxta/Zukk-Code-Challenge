9fd2f44951c31827f5cfb1f1199d1f242e6587e5
Matheus Costa <matheus271200.ms@gmail.com>
2020-12-18 22:46:11 -0300

    feat: :zap: creating new component Footer

---
+++ src/views/Home/Components/Footer/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {Foot, Button, ButtonText} from './styles';
+
+const Footer = () => {
+  return (
+    <>
+      <Foot>
+        <Button>
+          <ButtonText> Editar Layer </ButtonText>
+        </Button>
+      </Foot>
+    </>
+  );
+};
+
+export default Footer;
