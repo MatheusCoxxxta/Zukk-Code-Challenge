b6dfa1f6c3efcce7d6f4dde8df731d4d7ebb7bc8
Matheus Costa <matheus271200.ms@gmail.com>
2020-12-18 23:51:06 -0300

    feat: :zap: navigating to Drawer screen from Home

--- src/views/Home/Components/Footer/index.tsx
+++ src/views/Home/Components/Footer/index.tsx
@@ -1,13 +1,20 @@
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
 
 import {Foot, Button, ButtonText} from './styles';
 
 const Footer = () => {
+  const navigation = useNavigation();
   return (
     <>
       <Foot>
         <Button>
-          <ButtonText> Editar Layer </ButtonText>
+          <ButtonText
+            onPress={() => {
+              navigation.navigate('Drawer');
+            }}>
+            Editar Layer
+          </ButtonText>
         </Button>
       </Foot>
     </>
