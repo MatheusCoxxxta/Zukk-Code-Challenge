9cb5b0c28ea86d0b6db53d8c29d66fbed4432bee
Matheus Costa <matheus271200.ms@gmail.com>
2020-12-19 18:51:14 -0300

    feat: :clown_face: using id and coordinates format on mocked data

---
+++ src/mock/PointsArray.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-sparse-arrays
+const PointsArray = [
+  {id: 1, coordinates: [-45.88108891799865, -23.198347902841164]},
+  {id: 2, coordinates: [-45.87941218376059, -23.195613240125894]},
+  {id: 3, coordinates: [-45.882397835971034, -23.196415059549295]},
+  {id: 4, coordinates: [-45.87978, -23.19891]},
+];
+
+export default PointsArray;
