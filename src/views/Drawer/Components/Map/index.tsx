a09472da78700189088b07da2a9ec531fa545e04
Matheus Costa <matheus271200.ms@gmail.com>
2020-12-19 13:46:41 -0300

    feat: :sparkles: starting to try draw ne points in map on press

--- src/views/Drawer/Components/Map/index.tsx
+++ src/views/Drawer/Components/Map/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet} from 'react-native';
 
 import {View} from 'react-native';
@@ -6,30 +6,47 @@ import {View} from 'react-native';
 import {MapParentBox} from './styles';
 
 import MapboxGL from '@react-native-mapbox-gl/maps';
+import {Feature, Geometry, GeoJsonProperties} from 'geojson';
 
 MapboxGL.setAccessToken(
   'pk.eyJ1IjoibWF0aGV1c2Nvc3RhMjciLCJhIjoiY2tpdXo2MXNuMDU0bzJxcXR5NmF2bG1oeSJ9.pvbXRP9FDpW2IQYi77HM8w',
 );
 
-const points = [
-  [3.33624, 6.57901],
-  [3.3750014, 6.5367877],
-];
+const Map = (props: {pointsProps: any}) => {
+  const {pointsProps} = props;
 
-const Map = () => {
+  const [points, setPoints] = useState<any>(pointsProps);
+  const [point, setPoint] = useState<any>();
+
+  const handleSave = async (e: Feature<Geometry, GeoJsonProperties>) => {
+    setPoint(e);
+    setPoints({type: 'Feature', features: [...pointsProps.features, e]});
+    console.log({type: 'Feature', features: [...pointsProps.features, e]});
+  };
+
   return (
-    <View style={{flex: 1, height: '100%', width: '100%'}}>
-      <MapboxGL.MapView style={styles.map}>
-        <MapboxGL.Camera />
+    <View style={styles.mapGeneral}>
+      <MapboxGL.MapView onPress={(e) => handleSave(e)} style={styles.map}>
+        <MapboxGL.Camera
+          zoomLevel={15}
+          centerCoordinate={[-45.88108891799865, -23.198347902841164]}
+        />
         <MapParentBox>
-          {points.map((point) => (
-            <MapboxGL.PointAnnotation
-              key={point[0]}
-              id="pointAnnotation"
-              coordinate={point}>
-              <View style={styles.mapStyle} />
-            </MapboxGL.PointAnnotation>
-          ))}
+          <MapboxGL.ShapeSource
+            hitbox={{width: 10, height: 10}}
+            id="exampleShapeSource"
+            shape={points}>
+            <MapboxGL.CircleLayer
+              id="pointCircles"
+              // eslint-disable-next-line react-native/no-inline-styles
+              style={{
+                circleStrokeColor: '#FFF',
+                circleStrokeWidth: 2,
+                circleRadius: 15,
+                circleColor: '#459',
+              }}
+            />
+          </MapboxGL.ShapeSource>
         </MapParentBox>
       </MapboxGL.MapView>
     </View>
@@ -48,6 +65,7 @@ const styles = StyleSheet.create({
     borderColor: '#fff',
     borderWidth: 3,
   },
+  mapGeneral: {flex: 1, height: '100%', width: '100%'},
 });
 
 export default Map;
