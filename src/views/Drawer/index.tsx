a3ea3e5f4b7634a44049f4072a1da8f2e5f03435
Matheus Costa <matheus271200.ms@gmail.com>
2020-12-20 11:28:51 -0300

    feat: :zap: trying to send and get data from realm

--- src/views/Drawer/index.tsx
+++ src/views/Drawer/index.tsx
@@ -1,19 +1,56 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, StyleSheet, ScrollView, StatusBar} from 'react-native';
-
 import Header from './Components/Header';
 import Map from './Components/Map';
 import Footer from './Components/Footer';
-
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 import PointsArray from '../../mock/PointsArray';
+import getRealm from '../../services/realm';
 
 MapboxGL.setAccessToken(
   'pk.eyJ1IjoibWF0aGV1c2Nvc3RhMjciLCJhIjoiY2tpdXo2MXNuMDU0bzJxcXR5NmF2bG1oeSJ9.pvbXRP9FDpW2IQYi77HM8w',
 );
 
+const generateId = () => {
+  return Date.now() + Math.floor(Math.random() * Math.floor(99999));
+};
+
 const Home = () => {
+  useEffect(() => {
+    const getPoints = async () => {
+      const realm = await getRealm();
+
+      const data = realm.objects('Point');
+
+      console.log(data);
+    };
+
+    getPoints();
+  }, []);
+
+  const savePoint = async (lat: number, long: number) => {
+    const pointId = generateId();
+
+    const data = {
+      id: pointId,
+      lat,
+      long,
+    };
+
+    console.log(data);
+
+    const realm = await getRealm();
+
+    try {
+      realm.write(() => {
+        realm.create('Point', data);
+      });
+    } catch (error) {
+      console.log('Erro!');
+    }
+  };
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -22,7 +59,7 @@ const Home = () => {
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
           <Header />
-          <Map pointsProps={PointsArray} />
+          <Map pointsProps={PointsArray} savePoint={savePoint} />
           <Footer />
         </ScrollView>
       </SafeAreaView>
