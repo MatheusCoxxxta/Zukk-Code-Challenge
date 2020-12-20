22c20741b200f2df213047f51527e7e686f736b0
Matheus Costa <matheus271200.ms@gmail.com>
2020-12-20 17:19:55 -0300

    feat: :art: saving point on press "save"

--- src/views/Drawer/Components/Map/index.tsx
+++ src/views/Drawer/Components/Map/index.tsx
@@ -1,12 +1,10 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import {StyleSheet} from 'react-native';
-
 import {View} from 'react-native';
-
-import {MapParentBox} from './styles';
-
+import {MapParentBox, Foot, Button, ButtonText} from './styles';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 import {Feature, Geometry, GeoJsonProperties} from 'geojson';
+import {useNavigation} from '@react-navigation/native';
 
 MapboxGL.setAccessToken(
   'pk.eyJ1IjoibWF0aGV1c2Nvc3RhMjciLCJhIjoiY2tpdXo2MXNuMDU0bzJxcXR5NmF2bG1oeSJ9.pvbXRP9FDpW2IQYi77HM8w',
@@ -19,49 +17,65 @@ const Map = (props: {
   const {pointsProps} = props;
 
   const [point, setPoint] = useState<any>();
-
   const [pointLat, setPointLat] = useState<any>();
   const [pointLong, setPointLong] = useState<any>();
 
-  const handleSavePoint = async (e: Feature<Geometry, GeoJsonProperties>) => {
+  const navigation = useNavigation();
+
+  const creatingNewPoint = async (e: Feature<Geometry, GeoJsonProperties>) => {
     setPoint(e.geometry.coordinates); // lint bug => no sintaxe error
 
     setPointLat(e.geometry.coordinates[0]);
     setPointLong(e.geometry.coordinates[1]);
+  };
 
-    props.savePoint(pointLat, pointLong);
+  const handleSavePoint = () => {
+    console.log('Data: ', [pointLat, pointLong]);
+    if (pointLat && pointLong) {
+      props.savePoint(pointLat, pointLong);
+      navigation.goBack();
+    }
   };
 
   return (
-    <View style={styles.mapGeneral}>
-      <MapboxGL.MapView onPress={(e) => handleSavePoint(e)} style={styles.map}>
-        <MapboxGL.Camera
-          zoomLevel={15}
-          centerCoordinate={[-45.88108891799865, -23.198347902841164]}
-        />
-        <MapParentBox>
-          {pointsProps !== []
-            ? pointsProps.map((myPoint: any) => (
-                <MapboxGL.PointAnnotation
-                  key={myPoint.id}
-                  id="pointAnnotation"
-                  coordinate={[myPoint.lat, myPoint.long]}>
-                  <View style={styles.pointStyle} />
-                </MapboxGL.PointAnnotation>
-              ))
-            : null}
+    <>
+      <View style={styles.mapGeneral}>
+        <MapboxGL.MapView
+          onPress={(e) => creatingNewPoint(e)}
+          style={styles.map}>
+          <MapboxGL.Camera
+            zoomLevel={15}
+            centerCoordinate={[-45.88108891799865, -23.198347902841164]}
+          />
+          <MapParentBox>
+            {pointsProps !== []
+              ? pointsProps.map((myPoint: any) => (
+                  <MapboxGL.PointAnnotation
+                    key={myPoint.id}
+                    id="pointAnnotation"
+                    coordinate={[myPoint.lat, myPoint.long]}>
+                    <View style={styles.pointStyle} />
+                  </MapboxGL.PointAnnotation>
+                ))
+              : null}
 
-          {point ? (
-            <MapboxGL.PointAnnotation
-              key={point[0]}
-              id="pointAnnotation"
-              coordinate={point}>
-              <View style={styles.pointStyle} />
-            </MapboxGL.PointAnnotation>
-          ) : null}
-        </MapParentBox>
-      </MapboxGL.MapView>
-    </View>
+            {point ? (
+              <MapboxGL.PointAnnotation
+                key={point[0]}
+                id="pointAnnotation"
+                coordinate={point}>
+                <View style={styles.pointStyle} />
+              </MapboxGL.PointAnnotation>
+            ) : null}
+          </MapParentBox>
+        </MapboxGL.MapView>
+      </View>
+      <Foot>
+        <Button onPress={() => handleSavePoint()}>
+          <ButtonText> Salvar </ButtonText>
+        </Button>
+      </Foot>
+    </>
   );
 };
 
