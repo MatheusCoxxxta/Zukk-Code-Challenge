01a1a3c1309240ba68fd188e31f9004add419ed7
Matheus Costa <matheus271200.ms@gmail.com>
2020-12-20 17:17:23 -0300

    style: :art: using save button in map component

--- src/views/Drawer/Components/Map/styles.ts
+++ src/views/Drawer/Components/Map/styles.ts
@@ -4,4 +4,23 @@ const MapParentBox = styled.View`
   height: 500px;
 `;
 
-export {MapParentBox};
+const Foot = styled.View`
+  height: 100px;
+`;
+
+const Button = styled.TouchableOpacity`
+  margin: 20px 0;
+  height: 45px;
+  border-radius: 5px;
+  background-color: #459;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ButtonText = styled.Text`
+  color: #fff;
+  font-size: 14px;
+  font-weight: bold;
+`;
+
+export {MapParentBox, Foot, Button, ButtonText};
